f8ac267d7feccfd294374e6d242027f74bda8cb2
pedro araya <pedroaraya@fizz.cl>
2018-01-26 01:08:51 -0300

    cambios en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,29 @@ import { NgModule } from '@angular/core';
 //rutas
 
 import { APP_ROUTES } from './app.routes';
+//modulos
+import { PagesModule } from './pages/pages.module';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-import { PagesformComponent } from './login/pagesform.component';
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
+    LoginComponent,    
     RegisterComponent,
-    PagesformComponent
+  
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
