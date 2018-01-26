144a06d54041820c510cb893226a7e428589d584
pedro araya <pedroaraya@fizz.cl>
2018-01-25 23:57:08 -0300

    primer commit

---
+++ src/app/login/pagesform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagesform',
+  templateUrl: './pagesform.component.html',
+  styles: []
+})
+export class PagesformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
