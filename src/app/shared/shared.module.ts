33bb690efb57b91fb72b733364adf81ba8e75891
pedro araya <pedroaraya@fizz.cl>
2018-01-26 01:12:28 -0300

    cambios en la rutas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+
+import { NopagefoundComponent } from "./nopagefound/nopagefound.component";
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+    declarations:[
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+    ],
+    exports:[
+        NopagefoundComponent,
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+    ]
+})
+
+export class SharedModule{}
